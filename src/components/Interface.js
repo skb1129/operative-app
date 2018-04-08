8e572dd0c40d6da4501b95eb73faf2f64b8b420b
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-08 21:44:50 +0530

    feat: add some data in state

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
-const Interface = (props) => (
-  <div>
-    {JSON.stringify(props)}
-  </div>
-)
+class Interface extends Component {
+  state = {
+    data: {
+      portal1: [
+        {
+          user: 'p1test1',
+          message: 'Mah Nigga!'
+        }, {
+          user: 'p1test2',
+          message: 'Yo! Wassup Beyotch'
+        }, {
+          user: 'p1test3',
+          message: 'Hey! Dudes'
+        }
+      ],
+      portal2: [
+        {
+          user: 'p2test1',
+          message: 'Mah Nigga!'
+        }, {
+          user: 'p2test2',
+          message: 'Yo! Wassup Beyotch'
+        }, {
+          user: 'p2test3',
+          message: 'Hey! Dudes'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {JSON.stringify(this.state.data[this.props.match.params.portal])}
+      </div>
+    )
+  }
+}
 
 export default Interface;
