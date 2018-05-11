7f5f7612384ce7a461bf26aa5fa9d22c2cd88d76
Surya Kant <skb1129@yahoo.com>
2018-05-11 15:39:00 +0530

    refactor: deployment branch setup

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
 var path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 // viewed at http://localhost:8080
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 app.listen(8080);
